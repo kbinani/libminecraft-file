7b8eb6851a48da1594338f0731e60a9fd7d35df9
kbinani <kbinani.bt@gmail.com>
2019-05-29 01:57:13 +0900

    feat: Simulate light attenuation under water

--- test/main.cpp
+++ test/main.cpp
@@ -1,27 +1,197 @@
 #include <iostream>
 #include "minecraft-file.hpp"
+#include <math.h>
 #include <set>
 #include "svpng.inc"
 
 using namespace std;
 using namespace mcfile;
 
-static constexpr uint32_t RGB(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255) {
-    return ((uint32_t)a << 24) | ((uint32_t)b << 16) | ((uint32_t)g << 8) | (uint32_t)r;
-}
+class Color {
+public:
+    Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
+        : fR(r / 255.)
+        , fG(g / 255.)
+        , fB(b / 255.)
+        , fA(a / 255.)
+    {
+    }
 
-static map<string, uint32_t> const blockToColor {
-    {"minecraft:water", RGB(63, 63, 252)},
-    {"minecraft:stone", RGB(111, 111, 111)},
-    {"minecraft:granite", RGB(149, 108, 76)},
-    {"minecraft:diorite", RGB(252, 249, 242)},
-    {"minecraft:andesite", RGB(111, 111, 111)},
-    {"minecraft:chest", RGB(141,118,71)},
-    {"minecraft:clay", RGB(162,166,182)},
-    {"minecraft:coal_ore", RGB(111,111,111)},
-    {"minecraft:cobblestone", RGB(111,111,111)},
-    {"minecraft:dirt", RGB(149,108,76)},
-    {"minecraft:brown_mushroom", RGB(0,123,0)},
+    uint32_t color() const {
+        return ((uint32_t)0xFF << 24) | ((uint32_t)ToU8(fB * fA) << 16) | ((uint32_t)ToU8(fG * fA) << 8) | (uint32_t)ToU8(fR * fA);
+    }
+
+    Color diffuse(double diffusion, double distance) const {
+        double intensity = pow(10., -diffusion * distance);
+        return FromDouble(fR, fG, fB, fA * intensity);
+    }
+
+    Color withAlphaComponent(double a) const {
+        return FromDouble(fR, fG, fB, a);
+    }
+
+    static Color Add(Color a, Color b) {
+        return Color(a.fR * a.fA + b.fR * b.fA,
+                     a.fG * a.fA + b.fG * b.fA,
+                     a.fB * a.fA + b.fB * b.fA,
+                     1., true);
+    }
+
+    static Color FromDouble(double r, double g, double b, double a) {
+        return Color(r, g, b, a, true);
+    }
+
+    std::string toString() const {
+        std::ostringstream ss;
+        ss << "{R: " << fR << ", G: " << fG << ", B: " << fB << ", A: " << fA << "}";
+        return ss.str();
+    }
+
+private:
+    static uint8_t ToU8(double v) {
+        double vv = v * 255;
+        if (vv < 0) {
+            return 0;
+        } else if (255 < vv) {
+            return 255;
+        } else {
+            return (uint8_t)vv;
+        }
+    }
+
+    Color(double r, double g, double b, double a, bool)
+        : fR(r)
+        , fG(g)
+        , fB(b)
+        , fA(a)
+    {
+    }
+
+public:
+    double fR;
+    double fG;
+    double fB;
+    double fA;
+};
+
+static map<string, Color> const blockToColor {
+    {"minecraft:stone", Color(111, 111, 111)},
+    {"minecraft:granite", Color(149, 108, 76)},
+    {"minecraft:diorite", Color(252, 249, 242)},
+    {"minecraft:andesite", Color(111, 111, 111)},
+    {"minecraft:chest", Color(141, 118, 71)},
+    {"minecraft:clay", Color(162, 166, 182)},
+    {"minecraft:coal_ore", Color(111, 111, 111)},
+    {"minecraft:cobblestone", Color(111, 111, 111)},
+    {"minecraft:dirt", Color(149, 108, 76)},
+    {"minecraft:brown_mushroom", Color(0, 123, 0)},
+    {"minecraft:grass_block", Color(125, 176, 55)},
+    {"minecraft:iron_ore", Color(111, 111, 111)},
+    {"minecraft:sand", Color(149, 108, 76)},
+    {"minecraft:oak_leaves", Color(0, 123, 0)},
+    {"minecraft:jungle_leaves", Color(0, 123, 0)},
+    {"minecraft:birch_leaves", Color(67, 124, 37)},
+    {"minecraft:grass", Color(125, 176, 55)},
+    {"minecraft:vine", Color(0, 123, 0)},
+    {"minecraft:red_mushroom", Color(0, 123, 0)},
+    {"minecraft:mossy_cobblestone", Color(111, 111, 111)},
+    {"minecraft:oak_stairs", Color(127, 85, 48)},
+    {"minecraft:gravel", Color(111, 111, 1110)},
+    {"minecraft:oak_log", Color(141, 118, 71)},
+    {"minecraft:oak_planks", Color(127, 85, 48)},
+    {"minecraft:wall_torch", Color(255, 255, 2550)},
+    {"minecraft:farmland", Color(149, 108, 76)},
+    {"minecraft:oak_fence", Color(127, 85, 48)},
+    {"minecraft:cobblestone_stairs", Color(111, 111, 111)},
+    {"minecraft:black_wool", Color(25, 25, 25)},
+    {"minecraft:ladder", Color(255, 255, 255)},
+    {"minecraft:grass_path", Color(149, 108, 76)},
+    {"minecraft:birch_fence", Color(244, 230, 161)},
+    {"minecraft:birch_planks", Color(244, 230, 161)},
+    {"minecraft:birch_stairs", Color(244, 230, 161)},
+    {"minecraft:dark_oak_fence", Color(101, 75, 50)},
+    {"minecraft:dark_oak_log", Color(101, 75, 50)},
+    {"minecraft:dark_oak_planks", Color(101, 75, 50)},
+    {"minecraft:dark_oak_slab", Color(101, 75, 50)},
+    {"minecraft:dark_oak_stairs", Color(101, 75, 50)},
+    {"minecraft:dark_oak_trapdoor", Color(141, 118, 71)},
+    {"minecraft:diamond_ore", Color(111, 111, 111)},
+    {"minecraft:gold_ore", Color(111, 111, 111)},
+    {"minecraft:ice", Color(158, 158, 252)},
+    {"minecraft:jungle_fence", Color(149, 108, 76)},
+    {"minecraft:jungle_log", Color(149, 108, 76)},
+    {"minecraft:jungle_planks", Color(149, 108, 76)},
+    {"minecraft:jungle_slab", Color(149, 108, 76)},
+    {"minecraft:jungle_stairs", Color(149, 108, 76)},
+    {"minecraft:jungle_trapdoor", Color(141, 118, 71)},
+    {"minecraft:lapis_ore", Color(111, 111, 111)},
+    {"minecraft:lava", Color(252, 0, 0)},
+    {"minecraft:oak_door", Color(141, 118, 71)},
+    {"minecraft:oak_slab", Color(127, 85, 48)},
+    {"minecraft:oak_trapdoor", Color(141, 118, 71)},
+    {"minecraft:obsidian", Color(25, 25, 25)},
+    {"minecraft:packed_ice", Color(158, 158, 252)},
+    {"minecraft:polished_granite", Color(149, 108, 76)},
+    {"minecraft:prismarine", Color(75, 125, 151)},
+    {"minecraft:prismarine_bricks", Color(91, 216, 210)},
+    {"minecraft:rail", Color(255, 255, 255)},
+    {"minecraft:redstone_ore", Color(111, 111, 111)},
+    {"minecraft:sandstone", Color(244, 230, 161)},
+    {"minecraft:sea_lantern", Color(252, 249, 242)},
+    {"minecraft:snow", Color(252, 252, 252)},
+    {"minecraft:snow_block", Color(252, 252, 252)},
+    {"minecraft:spruce_door", Color(141, 118, 71)},
+    {"minecraft:spruce_fence", Color(141, 118, 71)},
+    {"minecraft:spruce_leaves", Color(0, 123, 0)},
+    {"minecraft:stone_brick_stairs", Color(111, 111, 111)},
+    {"minecraft:stone_bricks", Color(111, 111, 111)},
+    {"minecraft:stone_slab", Color(111, 111, 111)},
+    {"minecraft:spruce_log", Color(141, 118, 71)},
+    {"minecraft:spruce_planks", Color(127, 85, 48)},
+    {"minecraft:spruce_slab", Color(127, 85, 48)},
+    {"minecraft:spruce_stairs", Color(141, 118, 71)},
+    {"minecraft:spruce_trapdoor", Color(141, 118, 71)},
+    {"minecraft:mossy_stone_bricks", Color(111, 111, 111)},
+    {"minecraft:chiseled_stone_bricks", Color(111, 111, 111)},
+    {"minecraft:cracked_stone_bricks", Color(111, 111, 111)},
+    {"minecraft:infested_stone", Color(111, 111, 111)},
+    {"minecraft:cobweb", Color(255, 255, 255)},
+    {"minecraft:blue_ice", Color(102, 151, 246)},
+    {"minecraft:magma_block", Color(181, 64, 9)},
+
+    // plants
+    {"minecraft:lily_pad", Color(0, 123, 0)},
+    {"minecraft:wheat", Color(0, 123, 0)},
+};
+
+static set<string> plantBlocks = {
+    "minecraft:tall_grass",
+    "minecraft:beetroots",
+    "minecraft:carrots",
+    "minecraft:dandelion",
+    "minecraft:lilac",
+    "minecraft:melon",
+    "minecraft:orange_tulip",
+    "minecraft:oxeye_daisy",
+    "minecraft:peony",
+    "minecraft:pink_tulip",
+    "minecraft:poppy",
+    "minecraft:potatoes",
+    "minecraft:pumpkin",
+    "minecraft:rose_bush",
+    "minecraft:seagrass",
+    "minecraft:sugar_cane",
+    "minecraft:sunflower",
+    "minecraft:tall_seagrass",
+    "minecraft:fern",
+    "minecraft:azure_bluet",
+    "minecraft:allium",
+    "minecraft:blue_orchid",
+    "minecraft:kelp",
+    "minecraft:large_fern",
+    "minecraft:red_tulip",
+    "minecraft:white_tulip",
+    "minecraft:kelp_plant",
 };
 
 static set<string> transparentBlocks = {
@@ -43,16 +213,19 @@ int main(int argc, char *argv[]) {
 
     int const width = maxX - minX + 1;
     int const height = maxZ - minZ + 1;
-    vector<uint32_t> img(width * height, RGB(0, 0, 0, 0));
+    vector<uint32_t> img(width * height, Color(0, 0, 0, 0).color());
 
     World world(input);
 
-    set<string> unknownBlockNames;
+    map<string, size_t> unknownBlockNames;
 
     int const minRegionX = Coordinate::RegionFromBlock(minX);
     int const maxRegionX = Coordinate::RegionFromBlock(maxX);
     int const minRegionZ = Coordinate::RegionFromBlock(minZ);
     int const maxRegionZ = Coordinate::RegionFromBlock(maxZ);
+    Color const waterColor(63, 63, 252);
+    double const waterDiffusion = 0.02;
+
     for (int regionZ = minRegionZ; regionZ <= maxRegionZ; regionZ++) {
         for (int regionX = minRegionX; regionX <= maxRegionX; regionX++) {
             auto region = world.region(regionX, regionZ);
@@ -64,21 +237,45 @@ int main(int argc, char *argv[]) {
                 return data.load(stream, [&](Chunk const& chunk) {
                     for (int z = chunk.minZ(); z <= chunk.maxZ(); z++) {
                         for (int x = chunk.minX(); x <= chunk.maxX(); x++) {
+                            int waterDepth = 0;
+                            int airDepth = 0;
                             for (int y = 255; y >= 0; y--) {
                                 auto block = chunk.blockAt(x, y, z);
                                 if (!block) {
+                                    airDepth++;
                                     continue;
                                 }
-                                auto t = transparentBlocks.find(block->fName);
-                                if (t != transparentBlocks.end()) {
+                                if (block->fName == "minecraft:water" || block->fName == "minecraft:bubble_column") {
+                                    waterDepth++;
+                                    continue;
+                                }
+                                if (transparentBlocks.find(block->fName) != transparentBlocks.end()) {
+                                    airDepth++;
+                                    continue;
+                                }
+                                if (plantBlocks.find(block->fName) != plantBlocks.end()) {
+                                    airDepth++;
                                     continue;
                                 }
                                 auto it = blockToColor.find(block->fName);
                                 if (it == blockToColor.end()) {
-                                    unknownBlockNames.insert(block->fName);
+                                    auto u = unknownBlockNames.find(block->fName);
+                                    if (u == unknownBlockNames.end()) {
+                                        unknownBlockNames.insert(make_pair(block->fName, 1));
+                                    } else {
+                                        u->second += 1;
+                                    }
                                 } else {
+                                    Color const opaqeBlockColor = it->second;
+                                    uint32_t color;
+                                    if (waterDepth > 0) {
+                                        auto const w = waterColor.diffuse(waterDiffusion, waterDepth);
+                                        color = w.color();
+                                    } else {
+                                        color = opaqeBlockColor.color();
+                                    }
                                     int const index = (z - minZ) * width + (x - minX);
-                                    img[index] = it->second;
+                                    img[index] = color;
                                     break;
                                 }
                             }
@@ -91,7 +288,7 @@ int main(int argc, char *argv[]) {
     }
 
     for (auto it : unknownBlockNames) {
-        cerr << it << endl;
+        cerr << it.first << "\t" << it.second << endl;
     }
 
     FILE *out = fopen(output.c_str(), "wb");
