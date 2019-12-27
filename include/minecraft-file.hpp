63deb5ef08fb28070ca970616021cfce645c1392
kbinani <kbinani.bt@gmail.com>
2019-12-28 02:30:51 +0900

    Include filesystem namespace into mcfile::detail

--- include/minecraft-file.hpp
+++ include/minecraft-file.hpp
@@ -16,16 +16,53 @@
 #include <sstream>
 #include <limits>
 #include <functional>
-#if __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
+
+#if defined(__cpp_lib_filesystem)
+#  define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+#elif defined(__cpp_lib_experimental_filesystem)
+#  define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+#elif !defined(__has_include)
+#  define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+#elif __has_include(<filesystem>)
+#  ifdef _MSC_VER
+#    if __has_include(<yvals_core.h>) // for _HAS_CXX17
+#      include <yvals_core.h>
+#      if defined(_HAS_CXX17) && _HAS_CXX17
+#        define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+#      endif
+#    endif
+#    ifndef MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+#      define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
+#    endif
+#  else
+#    define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
+#  endif
+#elif __has_include(<experimental/filesystem>)
+#  define MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
 #else
-#include <filesystem>
+#  error Fatal: filesystem header not found
+#endif
+
+#if MCFILE_INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
+#  include <experimental/filesystem>
+namespace mcfile {
+namespace detail {
+    namespace filesystem = std::experimental::filesystem;
+} // namespace detail
+} // namespace mcfile
+#else
+#  include <filesystem>
+namespace mcfile {
+namespace detail {
+    namespace filesystem = std::filesystem;
+} // namespace detail
+} // namespace mcfile
 #endif
 
 #if defined(__APPLE__)
-#    include <libkern/OSByteOrder.h>
+#  include <libkern/OSByteOrder.h>
 #elif defined(__linux__) || defined(__CYGWIN__)
-#    include <endian.h>
+#  include <endian.h>
 #endif
 
 namespace mcfile {
@@ -4898,12 +4935,8 @@ public:
     }
 
     static bool concatCompressedNbt(int regionX, int regionZ, std::string const& directory, std::string const& resultMcaFilePath, std::function<std::string(int chunkX, int chunkZ)> name = Region::getDefaultCompressedChunkNbtFileName) {
-        #if __has_include(<experimental/filesystem>)
-        namespace fs = std::experimental::filesystem;
-        #else
-        namespace fs = std::filesystem;
-        #endif
-        
+        namespace fs = mcfile::detail::filesystem;
+
         int const minChunkX = regionX * 32;
         int const maxChunkX = minChunkX + 31;
         int const minChunkZ = regionZ * 32;
@@ -5000,11 +5033,8 @@ public:
     static bool iterateRegionForCompressedNbtFiles(std::string const& chunkFilesDirectory,
                                                    std::function<bool(int regionX, int regionZ, std::string const& chunkFilesDirectory)> callback,
                                                    std::function<std::string(int regionX, int regionZ)> regionFileName = Region::getDefaultRegionFileName) {
-        #if __has_include(<experimental/filesystem>)
-        namespace fs = std::experimental::filesystem;
-        #else
-        namespace fs = std::filesystem;
-        #endif
+        namespace fs = mcfile::detail::filesystem;
+
         int minRegionX = 1;
         int maxRegionX = -1;
         int minRegionZ = 1;
@@ -5174,11 +5204,7 @@ public:
     }
 
     bool eachRegions(std::function<void(std::shared_ptr<Region> const&)> callback) const {
-        #if defined(__cpp_lib_experimental_filesystem)
-        namespace fs = std::experimental::filesystem;
-        #else
-        namespace fs = std::filesystem;
-        #endif
+        namespace fs = mcfile::detail::filesystem;
         auto regionDir = fs::path(fRootDirectory).append("region");
         fs::directory_iterator it(regionDir);
         for (auto const& path : it) {
