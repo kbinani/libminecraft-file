45cd664c48aed012f82534c550e05da6c7c5b4f2
kbinani <kbinani.bt@gmail.com>
2020-10-18 00:22:11 +0900

    feat: ByteStream inherits the OutputStream

--- include/mcfile/stream/byte-stream.hpp
+++ include/mcfile/stream/byte-stream.hpp
@@ -3,12 +3,14 @@
 namespace mcfile {
 namespace stream {
 
-class ByteStream : public InputStream {
+class ByteStream : public InputStream, public OutputStream {
 public:
     explicit ByteStream(std::vector<uint8_t> &buffer)
             : fLoc(0) {
         this->fBuffer.swap(buffer);
     }
+
+    ByteStream() : fLoc(0) {}
 
     ~ByteStream() {}
 
@@ -17,11 +19,29 @@ public:
     ByteStream &operator=(ByteStream const &) = delete;
 
     bool read(void *buf, size_t size, size_t count) override {
-        if (fBuffer.size() <= fLoc + size * count) {
+        if (fBuffer.size() < fLoc + size * count) {
             return false;
         }
         std::copy(fBuffer.begin() + fLoc, fBuffer.begin() + fLoc + size * count, (uint8_t *) buf);
         fLoc += size * count;
+        return true;
+    }
+
+    bool write(void* buf, size_t size) override {
+        if (fBuffer.size() <= fLoc + size) {
+            size_t add = fLoc + size - fBuffer.size();
+            try {
+                for (int i = 0; i < add; i++) {
+                    fBuffer.push_back(0);
+                }
+            } catch (...) {
+                return false;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            fBuffer[fLoc + i] = ((uint8_t*)buf)[i];
+        }
+        fLoc += size;
         return true;
     }
 
@@ -38,6 +58,12 @@ public:
     bool valid() const override { return true; }
 
     long pos() const override { return fLoc; }
+
+    void drain(std::vector<uint8_t>& out) {
+        out.clear();
+        out.swap(fBuffer);
+        fLoc = 0;
+    }
 
 private:
     std::vector<uint8_t> fBuffer;
