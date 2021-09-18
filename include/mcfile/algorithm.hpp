143ef03f8ac2df28426074085afdb755aeed4263
kbinani <kbinani.bt@gmail.com>
2021-09-18 19:46:14 +0900

    fix: Fixed compile error on GCC

--- include/mcfile/algorithm.hpp
+++ include/mcfile/algorithm.hpp
@@ -3,9 +3,9 @@
 namespace mcfile {
 
 template<class R>
-inline auto Clamp(auto v) -> std::enable_if<std::is_integral<R>::value == std::is_integral<decltype(v)>::value && std::is_floating_point<R>::value == std::is_floating_point<decltype(v)>::value, R>::type {
+inline auto Clamp(auto v) -> typename std::enable_if<std::is_integral<R>::value == std::is_integral<decltype(v)>::value && std::is_floating_point<R>::value == std::is_floating_point<decltype(v)>::value, R>::type {
     using V = decltype(v);
-    using C = std::common_type<R, V>::type;
+    using C = typename std::common_type<R, V>::type;
     C constexpr min = static_cast<C>(std::numeric_limits<R>::lowest());
     C constexpr max = static_cast<C>(std::numeric_limits<R>::max());
     C clamped = std::min<C>(std::max<C>(v, min), max);
