b998b300f739432c80503b0af3557086f89f712f
kbinani <kbinani.bt@gmail.com>
2021-10-07 08:40:30 +0900

    feat: File::CreateTempDir

--- include/mcfile/file.hpp
+++ include/mcfile/file.hpp
@@ -98,6 +98,34 @@ public:
         return true;
     }
 
+    static std::optional<std::filesystem::path> CreateTempDir(std::filesystem::path const &tempDir) {
+        namespace fs = std::filesystem;
+        auto tmp = fs::temp_directory_path();
+#if defined(_MSC_VER)
+        wchar_t *dir = _wtempnam(tmp.native().c_str(), L"mcfile-tmp-");
+        if (dir) {
+            fs::path ret(dir);
+            fs::create_directory(ret);
+            free(dir);
+            return ret;
+        } else {
+            return std::nullopt;
+        }
+#else
+        using namespace std;
+        string tmpl("mcfile-tmp-XXXXXX");
+        vector<char> buffer;
+        copy(tmpl.begin(), tmpl.end(), back_inserter(buffer));
+        buffer.push_back(0);
+        char *dir = mkdtemp(buffer.data());
+        if (dir) {
+            return string(dir, strlen(dir));
+        } else {
+            return nullopt;
+        }
+#endif
+    }
+
 private:
     static std::string ModeString(Mode mode) {
         switch (mode) {
