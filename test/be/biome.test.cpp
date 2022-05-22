03131e5b0de78b75eed8baa60a7f73270a855782
kbinani <kbinani.bt@gmail.com>
2022-05-23 01:39:31 +0900

    fix: Fix logic error on test code

--- test/be/biome.test.cpp
+++ test/be/biome.test.cpp
@@ -9,7 +9,7 @@ using namespace mcfile::biomes;
 
 TEST_CASE("be/biome") {
     using namespace mcfile::biomes::minecraft;
-    for (BiomeId b = 1; b <= minecraft::minecraft_max_biome_id; b++) {
+    for (BiomeId b = 1; b < minecraft::minecraft_max_biome_id; b++) {
         if (b == minecraft::deep_dark || b == minecraft::mangrove_swamp) {
             //TODO: Not defined 1.19 BE yet
             continue;
