08eb8d65e209044b5858b7ed44ceeeb7b2544bb6
kbinani <kbinani.bt@gmail.com>
2023-01-04 23:49:33 +0900

    feat: Add McaBuilder

--- include/minecraft-file.hpp
+++ include/minecraft-file.hpp
@@ -109,6 +109,7 @@
 #include <mcfile/je/chunk.hpp>
 #include <mcfile/je/writable-chunk.hpp>
 #include <mcfile/je/mca-chunk-locator.hpp>
+#include <mcfile/je/mca-builder.hpp>
 #include <mcfile/je/region.hpp>
 #include <mcfile/je/world.hpp>
 
