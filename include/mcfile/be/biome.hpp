0eb187427e24b099908788f9ee00adc6983546d6
kbinani <kbinani.bt@gmail.com>
2024-06-09 10:38:28 +0900

    fix: Avoid converting unused biome for BE

--- include/mcfile/be/biome.hpp
+++ include/mcfile/be/biome.hpp
@@ -24,7 +24,7 @@ public:
         case 25: return stony_shore;
         case 26: return snowy_beach;
         case 4: return forest;
-        case 18: return wooded_hills;
+        case 18: return wooded_hills; // forest_hills not used in BE. replacing to wooded_hills
         case 132: return flower_forest;
         case 27: return birch_forest;
         case 28: return birch_forest_hills;
@@ -111,7 +111,7 @@ public:
         case lukewarm_ocean: return 41;
         case deep_lukewarm_ocean: return 44;
         case warm_ocean: return 40;
-        case deep_warm_ocean: return 43;
+        case deep_warm_ocean: return 40; // deep_warm_ocean=43 is not used in BE. replacing to warm_ocean=40
         case river: return 7;
         case frozen_river: return 11;
         case beach: return 16;
@@ -121,46 +121,45 @@ public:
         case wooded_hills: return 18; // forest_hills
         case flower_forest: return 132;
         case birch_forest: return 27;
-        case birch_forest_hills: return 28;
-        case old_growth_birch_forest: return 155; // birch_forest_mutated
-        case tall_birch_hills: return 156;        // birch_forest_hills_mutated
-        case dark_forest: return 29;              // roofed_forest
-        case dark_forest_hills: return 157;       // roofed_forest_mutated
+        case birch_forest_hills: return 27;       // birch_forest_hills=28 is not used in BE. replacing to birch_forest=27
+        case old_growth_birch_forest: return 155; // birch_forest_mutated=155
+        case tall_birch_hills: return 155;        // birch_forest_hills_mutated=156 is not used in BE. replacing to birch_forest_mutated=155
+        case dark_forest: return 29;              // roofed_forest=29
+        case dark_forest_hills: return 29;        // roofed_forest_mutated=157 is not used in BE. replacing to roofed_forest=29
         case jungle: return 21;
-        case jungle_hills: return 22;
-        case modified_jungle: return 149; // jungle_mutated
-        case sparse_jungle:
-            return 23;                         // jungle_edge
-        case modified_jungle_edge: return 151; // jungle_edge_mutated
-        case bamboo_jungle: return 21;         // 168;
-        case bamboo_jungle_hills: return 22;   // 169;
+        case jungle_hills: return 21;         // jungle_hills=22 is not used in BE. replacing to jungle=21
+        case modified_jungle: return 21;      // jungle_mutated=149 is not used in BE. replacing to jungle=21
+        case sparse_jungle: return 23;        // jungle_edge=23
+        case modified_jungle_edge: return 23; // jungle_edge_mutated=151 is not used in BE. replacing to jungle_edge=23
+        case bamboo_jungle: return 168;
+        case bamboo_jungle_hills: return 168; // bamboo_jungle_hills=169 is not used in BE. replacing to bamboo_jungle=168
         case taiga: return 5;
-        case taiga_hills: return 19;
-        case taiga_mountains: return 133;          // taiga_mutated
+        case taiga_hills: return 5;                // taig_hills=19 is not used in BE. replacing to taiga=5
+        case taiga_mountains: return 5;            //         taiga_mutated=133 is not used in BE. replacing to taiga=5
         case snowy_taiga: return 30;               // cold_taiga
-        case snowy_taiga_hills: return 31;         // cold_taiga_hills
-        case snowy_taiga_mountains: return 158;    // cold_taiga_mutated
+        case snowy_taiga_hills: return 30;         // cold_taiga_hills=31 is not used in BE. replacing to cold_taig=30
+        case snowy_taiga_mountains: return 30;     // cold_taiga_mutated=158 is not used in BE. replacing to cold_taig=30
         case old_growth_pine_taiga: return 32;     // mega_taiga
-        case giant_tree_taiga_hills: return 33;    // mega_taiga_hills
+        case giant_tree_taiga_hills: return 32;    // mega_taiga_hills=33 is not used in BE. replacing to mega_taiga=32
         case old_growth_spruce_taiga: return 160;  // redwood_taiga_mutated
-        case giant_spruce_taiga_hills: return 161; // redwood_taiga_hills_mutated
+        case giant_spruce_taiga_hills: return 160; // redwood_taiga_hills_mutated=161 is not used in BE. replacing to redwood_taiga_mutated=160
         case mushroom_fields: return 14;           // mushroom_island
-        case mushroom_field_shore: return 15;      // mushroom_island_shore
+        case mushroom_field_shore: return 14;      // mushroom_island_shore=15 is not used in BE. replacing to mushroom_island=14
         case swamp: return 6;                      // swampland
-        case swamp_hills: return 134;              // swampland_mutated
+        case swamp_hills: return 6;                // swampland_mutated=134 is not used in BE. replacing to swampland=6
         case savanna: return 35;
         case savanna_plateau: return 36;
         case windswept_savanna: return 163;         // savanna_mutated
-        case shattered_savanna_plateau: return 164; // savanna_plateau_mutated
+        case shattered_savanna_plateau: return 163; // savanna_plateau_mutated=164 is not used in BE. replacing to savanna_mutated=163
         case plains: return 1;
         case sunflower_plains: return 129;
         case desert: return 2;
-        case desert_hills: return 17;
-        case desert_lakes: return 130;   // desert_mutated
+        case desert_hills: return 2;     // desert_hills=17 is not used in BE. replacing to desert=2
+        case desert_lakes: return 2;     // desert_mutated=130 is not used in BE. replacing to desert=2
         case snowy_plains: return 12;    // ice_plains
-        case snowy_mountains: return 13; // ice_mountains
+        case snowy_mountains: return 12; // ice_mountains=13 is not used in BE. replacing to ice_plains=12
         case ice_spikes: return 140;     // ice_plains_spikes
-        case windswept_hills:
+        case windswept_hills:            // extreme_hills (listed as windswept_hills on wiki)
         case mountain_edge:
             return 3;                     // extreme_hills
         case windswept_forest: return 34; // extreme_hills_plus_trees
@@ -169,12 +168,12 @@ public:
             return 131; // extreme_hills_mutated
         // case extreme_hills_plus_trees_mutated: return 162;
         // case extreme_hills_edge: return 20;
-        case badlands: return 37;                          // mesa
-        case badlands_plateau: return 38;                  // mesa_plateau
-        case modified_badlands_plateau: return 166;        // mesa_plateau_mutated
-        case wooded_badlands: return 39;                   // mesa_plateau_stone
-        case modified_wooded_badlands_plateau: return 167; // mesa_plateau_stone_mutated
-        case eroded_badlands: return 165;                  // mesa_bryce
+        case badlands: return 37;                         // mesa
+        case badlands_plateau: return 37;                 // mesa_plateau=38 is not used in BE. replacing mesa=37
+        case modified_badlands_plateau: return 37;        // mesa_plateau_mutated=166 is not used in BE. replacing mesa=37
+        case wooded_badlands: return 38;                  // mesa_plateau_stone=38 (wiki says 39, actually 38)
+        case modified_wooded_badlands_plateau: return 38; // mesa_plateau_stone_mutated=167 is not used in BE. replacing to mesa_plateau_stone=38
+        case eroded_badlands: return 165;                 // mesa_bryce
         case meadow: return 186;
         case grove: return 185;
         case snowy_slopes: return 184;
